99749ad9c8b675229d52ec9a320df7a2260ee3e7
Berat <yunus-berat1@hotmail.com>
2022-03-19 19:45:35 +0300

    Assignment3

--- app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.clickcounter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // sets up the count variable and sets it to 0 //
     var count = 0
 
+    // overrides and starts the activity //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    // setting up the function of the button so the counter goes up by 1 whenever we click //
     fun onTap(view: View){
         count++
 
+        // makes the text change whenever the value of count changes  //
         val textview = findViewById(R.id.textView) as TextView
         textview.text = "You clicked $count times"
     }
